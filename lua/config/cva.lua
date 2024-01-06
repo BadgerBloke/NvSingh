386e8f12660d95e1abb3c9a0424a1eb16fc0a247
Mukesh Singh <camukeshkumarsingh@gmail.com>
2024-01-06 15:22:36 +0530

    initial commit

---
+++ lua/config/cva.lua
@@ -0,0 +1,18 @@
+local M = {}
+
+M.setup = function()
+  require("lspconfig").tailwindcss.setup({
+    settings = {
+      tailwindCSS = {
+        experimental = {
+          classRegex = {
+            { "cva\\(([^)]*)\\)", "[\"'`]([^\"'`]*).*?[\"'`]" },
+            { "cx\\(([^)]*)\\)", "(?:'|\"|`)([^']*)(?:'|\"|`)" },
+          },
+        },
+      },
+    },
+  })
+end
+
+return M
