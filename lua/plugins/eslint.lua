13feb87b2cd1028c521e6217879b2ebdd9baab8a
Mukesh Singh <CAMukeshKumarSingh@gmail.com>
2025-02-23 04:04:27 +0530

    eslint config updated

--- lua/plugins/eslint.lua
+++ lua/plugins/eslint.lua
@@ -5,16 +5,34 @@ return {
     servers = {
       eslint = {
         settings = {
+          -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
           workingDirectories = { mode = "auto" },
+          -- Enable auto fix on save
+          codeAction = {
+            disableRuleComment = {
+              enable = true,
+              location = "separateLine",
+            },
+            showDocumentation = {
+              enable = true,
+            },
+          },
           codeActionOnSave = {
             enable = true,
             mode = "all",
           },
           format = true,
+          nodePath = "",
+          onIgnoredFiles = "off",
+          packageManager = "npm",
+          quiet = false,
           rulesCustomizations = {},
           run = "onType",
+          useESLintClass = false,
           validate = "on",
-          workingDirectory = { mode = "location" },
+          workingDirectory = {
+            mode = "location",
+          },
         },
       },
     },
@@ -26,7 +44,7 @@ return {
 
         local formatter = require("lazyvim.util").lsp.formatter({
           name = "eslint: lsp",
-          primary = true,
+          primary = true, -- Set to true to make it primary formatter
           priority = 200,
           filter = "eslint",
         })
@@ -34,16 +52,13 @@ return {
         -- Register the formatter with LazyVim
         require("lazyvim.util").format.register(formatter)
 
-        -- Ensure ESLint fixes issues on save
+        -- Add additional setup for import sorting
         vim.api.nvim_create_autocmd("BufWritePre", {
           pattern = { "*.js", "*.jsx", "*.ts", "*.tsx" },
           callback = function(args)
             local client = get_client(args.buf)
             if client then
-              vim.lsp.buf.execute_command({
-                command = "_eslint.applyAllFixes",
-                arguments = { { uri = vim.uri_from_bufnr(args.buf) } },
-              })
+              vim.cmd("EslintFixAll")
             end
           end,
         })
