af806c0ab80f271db54669aa019886a251037dfb
Mukesh Singh <CAMukeshKumarSingh@gmail.com>
2025-02-23 14:03:53 +0530

    fixing cursor flickering in react project

---
+++ lua/plugins/supermaven.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "supermaven-inc/supermaven-nvim",
+    lazy = false,
+    config = function()
+      require("supermaven-nvim").setup({
+        keymaps = {
+          accept_suggestion = "<tab>",
+          clear_suggestion = "<C-]>",
+          accept_word = "<C-j>",
+        },
+        ignore_filetypes = { cpp = true }, -- or { "cpp", }
+        color = {
+          suggestion_color = "#ff8800",
+          cterm = 244,
+        },
+        log_level = "info", -- set to "off" to disable logging completely
+        disable_inline_completion = false, -- disables inline completion for use with cmp
+        disable_keymaps = false, -- disables built in keymaps for more manual control
+        condition = function()
+          return false
+        end, -- condition to check for stopping supermaven, `true` means to stop supermaven when the condition is true.
+      })
+    end,
+  },
+}
