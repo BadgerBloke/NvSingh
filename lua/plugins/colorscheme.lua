9147ae5eebcd437f18cfce1ef64bf992d9d035cb
Mukesh Singh <CAMukeshKumarSingh@gmail.com>
2025-03-29 03:53:57 +0530

    feat: colorscheme updated

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -17,7 +17,8 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      -- colorscheme = "catppuccin",
+      colorscheme = "tokyonight-night",
     },
   },
 }
