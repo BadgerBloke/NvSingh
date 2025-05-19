13dc6f27ed6f820a7c6db71ebe1fe10f9a47e763
Mukesh Singh <CAMukeshKumarSingh@gmail.com>
2025-05-19 21:40:16 +0530

    feat: diagnostic-float plugin added

---
+++ lua/plugins/diagnostic-float.lua
@@ -0,0 +1,29 @@
+-- In your plugins config
+return {
+  {
+    "BadgerBloke/diagnostic-float.nvim",
+    dependencies = { "neovim/nvim-lspconfig" },
+    opts = {
+      enabled = false,
+      delay = 2000,
+      toggle_key = "<C-i>",
+      leader_command = "xd",
+    },
+    keys = {
+      {
+        "<C-i>",
+        function()
+          require("diagnostic-float").show_diagnostic_float()
+        end,
+        desc = "Show diagnostic",
+      },
+      {
+        "<leader>xd",
+        function()
+          require("diagnostic-float").toggle()
+        end,
+        desc = "Toggle diagnostic",
+      },
+    },
+  },
+}
