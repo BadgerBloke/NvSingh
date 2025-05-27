5b38c765f110a2f0763209a8b21d672fe824e72e
Mukesh Singh <CAMukeshKumarSingh@gmail.com>
2025-05-27 10:24:40 +0530

    fix: dap fixes for javascript and rust

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,27 +1,30 @@
 return {
   {
     "mrcjkb/rustaceanvim",
-    version = "^5", -- Recommended
+    -- version = "^5", -- Recommended
     lazy = false, -- This plugin is already lazy
     ft = "rust",
     config = function()
       local mason_registry = require("mason-registry")
-      -- Updated: Using $MASON instead of get_install_path()
-      local extension_path = vim.fn.expand("$MASON/packages/codelldb/extension/")
+      local codelldb = mason_registry.get_package("codelldb")
+      local extension_path = codelldb:get_install_path() .. "/extension/"
       local codelldb_path = extension_path .. "adapter/codelldb"
-      local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
-      -- If you are on Linux, replace the line above with the line below:
-      -- local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
+
+      -- Cross-platform liblldb path detection
+      local liblldb_path
+      if vim.fn.has("mac") == 1 then
+        liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
+      elseif vim.fn.has("unix") == 1 then
+        liblldb_path = extension_path .. "lldb/lib/liblldb.so"
+      elseif vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
+        liblldb_path = extension_path .. "lldb/bin/liblldb.dll"
+      else
+        -- Fallback to Linux if platform detection fails
+        liblldb_path = extension_path .. "lldb/lib/liblldb.so"
+      end
+
       local cfg = require("rustaceanvim.config")
       vim.g.rustaceanvim = {
-        cmd = function()
-          local mason_registry = require("mason-registry")
-          local ra_binary = mason_registry.is_installed("rust-analyzer")
-              -- Using $MASON environment variable instead of get_install_path()
-              and vim.fn.expand("$MASON/bin/rust-analyzer")
-            or "rust-analyzer"
-          return { ra_binary } -- You can add args to the list, such as '--log-file'
-        end,
         dap = {
           adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
         },
@@ -35,8 +38,8 @@ return {
   --   init = function()
   --     vim.g.rustfmt_autosave = 1
   --   end,
-  --   url = "git@github.com:rust-lang/rust.vim.git",
   -- },
+
   {
     "saecki/crates.nvim",
     ft = { "toml" },
@@ -47,6 +50,9 @@ return {
             enabled = true,
           },
         },
+      })
+      require("cmp").setup.buffer({
+        sources = { { name = "crates" } },
       })
     end,
   },
