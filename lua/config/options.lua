348791dfacde1f166267cb20e87ec60d47e0709b
Mukesh Singh <CAMukeshKumarSingh@gmail.com>
2025-09-19 16:25:05 +0530

    feat: added opencode plugin

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -12,3 +12,16 @@ vim.g.lazyvim_prettier_needs_config = true
 vim.opt.winbar = "%=%m %f"
 
 vim.opt.shiftwidth = 4 -- Set the number of spaces for each step of (auto)indent
+
+-- Listen for opencode events
+vim.api.nvim_create_autocmd("User", {
+  pattern = "OpencodeEvent",
+  callback = function(args)
+    -- See the available event types and their properties
+    vim.notify(vim.inspect(args.data))
+    -- Do something interesting, like show a notification when opencode finishes responding
+    if args.data.type == "session.idle" then
+      vim.notify("opencode finished responding")
+    end
+  end,
+})
