7c24dfe77d4d496e13521178814185f37fce0a78
Mukesh Singh <CAMukeshKumarSingh@gmail.com>
2025-09-19 17:11:04 +0530

    fix: opencode notifications disabled for generic

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -18,7 +18,7 @@ vim.api.nvim_create_autocmd("User", {
   pattern = "OpencodeEvent",
   callback = function(args)
     -- See the available event types and their properties
-    vim.notify(vim.inspect(args.data))
+    -- vim.notify(vim.inspect(args.data))
     -- Do something interesting, like show a notification when opencode finishes responding
     if args.data.type == "session.idle" then
       vim.notify("opencode finished responding")
